ffe9ed1cd78aada33261e1d838138892383fc517
Turbobent <kasp728h@edu.mercantec.dk>
2025-04-23 09:46:46 +0200

    appDB

---
+++ API/Data/Using.cs
@@ -0,0 +1,17 @@
+﻿global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using System.ComponentModel.DataAnnotations;
+global using Microsoft.AspNetCore.Authorization;
+global using System.IdentityModel.Tokens.Jwt;
+global using Microsoft.IdentityModel.Tokens;
+global using System.Text.RegularExpressions;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.OpenApi.Models;
+global using System.Security.Claims;
+global using System.Text;
+global using API.Models;
+global using API.Data;
+global using System.Text.Json;
+global using System.Data;
+
