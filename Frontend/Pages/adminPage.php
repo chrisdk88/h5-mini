b38a4237103f8ad7ec58a0f14c90d6b143d9f549
Manne0010 <157686735+Manne0010@users.noreply.github.com>
2025-04-28 10:10:00 +0200

    added adminpage and folder 'pages'

---
+++ Frontend/Pages/adminPage.php
@@ -0,0 +1,18 @@
+<?php 
+  include_once($_SERVER['DOCUMENT_ROOT'] . "/H5-MINI/Frontend/templates/header.php") 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Admin</title>
+</head>
+
+<body>
+  <h1>Admin Page</h1>
+</body>
+
+</html>
