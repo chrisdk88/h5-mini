e57a7ef848ae56bfa25107069d6513efe2ae86b9
Manne0010 <157686735+Manne0010@users.noreply.github.com>
2025-04-28 13:27:50 +0200

    createt pages for editing leadboard and statistics, and also for user administration page

---
+++ Frontend/Pages/AdminPages/leaderboardAndStatisticsAdministration.php
@@ -0,0 +1,21 @@
+<?php 
+  include_once($_SERVER['DOCUMENT_ROOT'] . "/H5-MINI/Frontend/templates/header.php") 
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Leaderboard/statistics Editing</title>
+</head>
+
+<body class="bg-[#006400] text-white">
+  <h1>Edit Leaderboard or statistics</h1>
+
+
+  
+</body>
+
+</html>
