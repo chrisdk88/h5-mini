0db656aacf507ca5f02ef64d221c57c9c98a8604
TGSXVII <madsduly@gmail.com>
2025-05-15 14:33:25 +0200

    more bs

--- Frontend/includes/links.php
+++ Frontend/includes/links.php
@@ -1,6 +1,12 @@
 <!-- All needed links -->
-<?php $baseAPI = "https://dles-api.mercantec.tech/api/" ?>
+<?php $baseAPI = "https://dles-api.mercantec.tech/api/"
+?>
 <?php $baseURL = "http://localhost/h5-mini/Frontend/" ?>
 <?php $baseDataURL = "http://localhost/h5-mini/Frontend/Admin/DataSorting/" ?>
+
 <!-- tailwind -->
 <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+
+<!-- Backup -->
+<?php //$baseAPI = "http://localhost:5014/api/" 
+?>
